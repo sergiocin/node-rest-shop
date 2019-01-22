6b4c426364a11b4303dd3b0fa331bf1abec12e15
Sérgio Carvalho <sergio.carvalho@outlook.com>
2019-01-22 17:47:59 -0200

    Sets CORS config

--- src/app.js
+++ src/app.js
@@ -9,6 +9,17 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.use('/', routes)
 
 // Handling errors
