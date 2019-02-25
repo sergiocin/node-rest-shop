dea1eb943a22b6c661b22e0f0d83c9d617bac7e7
Sérgio Carvalho <sergio.carvalho@outlook.com>
2019-02-25 02:13:41 +0000

    Creates a working signup route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 
 import orders from './orders'
 import products from './products'
+import users from './users'
 
 const router = express.Router()
 
 router.use('/orders', orders)
 router.use('/products', products)
+router.use('/users', users)
 
 // Handling not found requests
 router.use((req, res, next) => {
---
+++ src/routes/users.js
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+import User from './../models/user'
+
+const router = Router()
+
+router.post('/signup', async (req, res) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: hash
+    })
+
+    const result = await user.save()
+    if (result) {
+      res.status(201).json({
+        message: 'Okay, we have created the user'
+      })
+    }
+  } catch (error) {
+    console.log('Hash Error: ', error)
+    res.status(500).json({
+      error
+    })
+  }
+})
+
+export default router
